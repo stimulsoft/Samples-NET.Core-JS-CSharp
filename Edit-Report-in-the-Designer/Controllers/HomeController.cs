0d349616362ff52658b703836a7b9a40481ae7f0
Vladimir <hotchenko@gmail.com>
2017-01-07 11:59:52 +0300

    add new samples

---
+++ Edit-Report-in-the-Designer/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Stimulsoft.Report.NetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Edit_Report_in_the_Designer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostEnvironment;
+
+        public HomeController(IHostingEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult GetReport()
+        {
+            return StiNetCoreDesigner.GetReportResult(this, _hostEnvironment.WebRootPath + "\\reports\\SimpleList.mrt");
+        }
+
+        public IActionResult GetReportData()
+        {
+            return StiNetCoreDesigner.GetReportDataResult(this, _hostEnvironment.WebRootPath + "\\data\\Demo.json");
+        }
+
+        public IActionResult DesignerEvent()
+        {
+            return StiNetCoreDesigner.DesignerEventResult(this);
+        }
+    }
+}
---
+++ Show-Report-in-the-Viewer/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Stimulsoft.Report.NetCore;
+
+namespace Show_Report_in_the_Viewer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostEnvironment;
+
+        public HomeController(IHostingEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult GetReport()
+        {
+            return StiNetCoreViewer.GetReportResult(this, _hostEnvironment.WebRootPath + "\\reports\\SimpleList.mrt");
+        }
+
+        public IActionResult GetReportData()
+        {
+            return StiNetCoreViewer.GetReportDataResult(this, _hostEnvironment.WebRootPath + "\\data\\Demo.json");
+        }
+
+        public IActionResult ViewerEvent()
+        {
+            return StiNetCoreViewer.ViewerEventResult(this);
+        }
+    }
+}
